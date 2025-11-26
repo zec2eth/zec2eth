76a9117b8137fbe41465b15e4e96305e61f1924d
cozfuttu <canozfuttu07@gmail.com>
2025-11-26 21:38:45 +0300

    chore: renamed scripts to add indexes

--- circuits/scripts/3_setup-groth16.sh
+++ circuits/scripts/3_setup-groth16.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ceremony
-snarkjs groth16 setup ../build/main.r1cs pot12_final.ptau main_0000.zkey
+snarkjs groth16 setup ../build/main.r1cs pot22_final.ptau main_0000.zkey
 snarkjs zkey contribute main_0000.zkey main.zkey
 snarkjs zkey export verificationkey main.zkey verification_key.json
